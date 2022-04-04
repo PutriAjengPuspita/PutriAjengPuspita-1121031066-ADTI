4ff810a44dce1c2e80d6e280e582198c015a1b32
PutriAjengPuspita <102979281+PutriAjengPuspita@users.noreply.github.com>
2022-04-04 22:48:55 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+//nama: putri ajeng puspita
+//nim: 1121031066
+//kelas: B
+
+int main()
+{
+        int HH,MM,SS,a,b;
+    b=a=0;
+    while(b==0)
+    {
+    cout<<"set jam : "<<endl;
+    cin>>HH;
+    cout<<"set menit : "<<endl;
+    cin>>MM;
+    cout<<"set detik: "<<endl;
+    cin>>SS;
+    if (HH<<23 && MM << 59 && SS <<59)
+    b++;
+    else
+        system ("cls");
+    }
+    while (a==0)
+        {
+        system ("cls");
+        cout << HH << ":" << MM << ":" << SS << endl;
+        SS++;
+        if (SS>59)
+        {
+            SS=0;
+       MM++;
+        }
+
+        if (MM > 59)
+        {
+            MM=0;
+        HH++;
+        }
+        if (HH > 23)
+        {
+            HH=0;
+        }
+        }
+        return 0;
+}
